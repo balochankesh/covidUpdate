10e0fb864869e3a59c3532f5fca8748a37a175e0
ankesh baloch <balochankesh@gmail.com>
2020-05-13 13:32:11 +0530

    no css yet

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask,request,jsonify,redirect,render_template,url_for
+import requests
+
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST","GET"])
+
+def home():
+
+    if request.method == "POST":
+        se = request.form["state"]
+
+        return redirect(url_for("state",state=se))
+    else:
+        return render_template("home.html")
+
+
+@app.route("/<state>/")
+def state(state):
+    data = requests.get("https://souravcovid19api.herokuapp.com/api/state/"+state)
+    r= data.json()
+
+    name = r["Name"]
+    conf = r["Confirmed"]
+    deaths = r["Death"]
+    cured = r["Cured"]
+
+    return render_template("state.html", state=name,conf=conf,deaths=deaths,cured=cured)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
